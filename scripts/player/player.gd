1df2ff432bee4d826ca336278a2cf088d4a4bcb7
santimattiassi <santiago.mattiassi@gmail.com>
2026-06-22 01:44:36 -0300

    feat: player animations, Idle and running

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,9 +1,17 @@
 extends CharacterBody3D
 
-@export var speed := 5.0
+@export var speed := 4.0
+@export var run_speed := 7.0
+
+@onready var animated_knight = $AnimatedKnight
+@onready var animation_player = $AnimatedKnight/AnimationPlayer
 
 func _physics_process(_deltaa):
+	var current_speed = speed
 
+	if Input.is_key_pressed(KEY_SHIFT):
+		current_speed = run_speed
+		
 	var input_dir := Vector2.ZERO
 
 	if Input.is_action_pressed("ui_right"):
@@ -24,10 +32,27 @@ func _physics_process(_deltaa):
 		input_dir.y
 	).normalized()
 
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	velocity.x = direction.x * current_speed
+	velocity.z = direction.z * current_speed
+	
+	if direction.length() > 0:
 
-	#if direction.length() > 0:
-		#look_at(global_position + direction, Vector3.UP)
+		if current_speed == run_speed:
+			if animation_player.current_animation != "/Player/Running_B":
+				animation_player.play("Player/Running_B")
+		else:
+
+			if animation_player.current_animation != "/Player/Walking_A":
+				animation_player.play("Player/Walking_A")
+		
+		animated_knight.look_at(
+			animated_knight.global_position - direction,
+			Vector3.UP
+		)
+	else:
+		if animation_player.current_animation != "/Player/Idle_A":
+			animation_player.play("Player/Idle_A")
+	
+		
 
 	move_and_slide()
